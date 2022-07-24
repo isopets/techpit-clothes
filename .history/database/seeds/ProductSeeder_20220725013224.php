4acbfa4c17a1eaec1ab2d00d61f3f2b6cb7b272b
yuto isogai <jandp.0717@gmail.com>
2022-07-25 01:40:44 +0900

    seeder create & seeder add

---
+++ .history/database/seeds/ProductSeeder_20220725013224.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->inert([
+            [
+                'name' => 'Black T-shirt',
+                'description' => 'コットン素材を使用したクールネックのカットソー。',
+                'image' =>'/images/1.jpg',
+                'price' => '4500'
+            ],
+            [
+                'name' => 'White T-shirt','descri'
+
+            ]
+        ])
+    }
+}
